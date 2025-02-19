de07cd3d7d7e516e58e1fc9dd18388a38fe46453
Alejandro Pérez <jandropehe4@gmail.com>
2025-02-19 21:48:05 +0100

    Fix VersionCode = 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.jatora.tfg_the_climb_within"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
