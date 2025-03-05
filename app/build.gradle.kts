915a890483415b22fc56174723fd0d3621d96ce6
Tom <garciatomaschavez@gmail.com>
2025-03-05 11:43:40 +0100

    Updated version mode to alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         targetSdk = 34
         versionCode = 2
         versionName = "0.2"
-        versionNameSuffix = "b"
+        versionNameSuffix = "a"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
